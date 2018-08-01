d69248e0d5e136e084e43e3cd9d09393f8253c5f
John George <johngeorge.ctprice@gmail.com>
2018-08-02 00:25:00 +0530

    set up express

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
+/* eslint-disable */
+
+const express= require('express');
+const bodyParser= require('body-parser');
+const cors= require('cors');
+const morgan= require('morgan');
+
 console.log('yo bois');
+const app=express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/status' , (req,res) =>{
+  res.send( {
+    message: 'hello world'
+  });
+} )
+app.listen(process.env.PORT || 8081);
